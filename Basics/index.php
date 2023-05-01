11881e18a9d18fe9fc3d7134e0f5ef4a11b94c9e
Kareem Shehab <kshehab1@outlook.com>
2023-05-01 12:26:50 -0700

    Setting up variables and functions

--- Basics/index.php
+++ Basics/index.php
@@ -1,15 +1,25 @@
+<?php
+// Working with variables
+$firstVar = "Kareem";
+$firstFunction = function ($firstParam) {
+    echo "Hello {$firstParam}!";
+};
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="description" content="PHP Basics">
-    <meta name=viewport content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>PHP Basics</title>
-  </head>
-  
-  <body>
-    <h1>Todays Message is:</h1>
-    <p><?php echo "Hello buds!"; ?></p>
-  </body>
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="PHP Basics">
+		<meta name=viewport content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>PHP Basics</title>
+	</head>
+
+	<body>
+		<h1>Todays Message is:</h1>
+		<p><?php $firstFunction($firstVar);?></p>
+	</body>
+
 </html>
